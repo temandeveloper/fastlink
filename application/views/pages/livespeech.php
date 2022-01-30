346a4d95454a4c75cfde3445b9c024f5ce353cbc
AhmadFadil <jackhemer123@gmail.com>
2022-01-30 21:20:14 +0700

    last test

--- application/views/pages/livespeech.php
+++ application/views/pages/livespeech.php
@@ -78,7 +78,31 @@
             <input type="text" class="form-control" id="target" placeholder="Target">
         </div>
         <label for="formControlRange" style="color:white">Voice</label>
-        <select id="voice-select" class="form-control form-control-sm"></select>
+        <!-- <select id="voice-select" class="form-control form-control-sm"></select> -->
+        <select id="voice-select" class="form-control form-control-sm">
+            <option data-lang="en-US" data-name="Microsoft David - English (United States)">Microsoft David - English (United States)(en-US)</option>
+            <option data-lang="en-US" data-name="Microsoft Mark - English (United States)">Microsoft Mark - English (United States)(en-US)</option>
+            <option data-lang="en-US" data-name="Microsoft Zira - English (United States)">Microsoft Zira - English (United States)(en-US)</option>
+            <option data-lang="de-DE" data-name="Google Deutsch">Google Deutsch(de-DE)</option><option data-lang="en-US" data-name="Google US English">Google US English(en-US)</option>
+            <option data-lang="en-GB" data-name="Google UK English Female">Google UK English Female(en-GB)</option>
+            <option data-lang="en-GB" data-name="Google UK English Male">Google UK English Male(en-GB)</option>
+            <option data-lang="es-ES" data-name="Google español">Google español(es-ES)</option>
+            <option data-lang="es-US" data-name="Google español de Estados Unidos">Google español de Estados Unidos(es-US)</option>
+            <option data-lang="fr-FR" data-name="Google français">Google français(fr-FR)</option>
+            <option data-lang="hi-IN" data-name="Google हिन्दी">Google हिन्दी(hi-IN)</option>
+            <option data-lang="id-ID" data-name="Google Bahasa Indonesia">Google Bahasa Indonesia(id-ID)</option>
+            <option data-lang="it-IT" data-name="Google italiano">Google italiano(it-IT)</option>
+            <option data-lang="ja-JP" data-name="Google 日本語">Google 日本語(ja-JP)</option>
+            <option data-lang="ko-KR" data-name="Google 한국의">Google 한국의(ko-KR)</option>
+            <option data-lang="nl-NL" data-name="Google Nederlands">Google Nederlands(nl-NL)</option>
+            <option data-lang="pl-PL" data-name="Google polski">Google polski(pl-PL)</option>
+            <option data-lang="pt-BR" data-name="Google português do Brasil">Google português do Brasil(pt-BR)</option>
+            <option data-lang="ru-RU" data-name="Google русский">Google русский(ru-RU)</option>
+            <option data-lang="zh-CN" data-name="Google 普通话（中国大陆）">Google 普通话（中国大陆）(zh-CN)</option>
+            <option data-lang="zh-HK" data-name="Google 粤語（香港）">Google粤語（香港）(zh-HK)</option>
+            <option data-lang="zh-TW" data-name="Google 國語（臺灣）">Google 國語（臺灣）(zh-TW)</option>
+        </select>
+
         <div class="input-group">
             <label for="formControlRange" style="color:white">Rate</label>
             <input type="range" class="form-control-range" id="rate" min="0" max="2" value="1" step="0.1">
@@ -143,25 +167,25 @@
         const getVoices = () => {
         voices = synth.getVoices();
 
-            // Loop through voices and create an option for each one
-            voices.forEach(voice => {
-                // Create option element
-                const option = document.createElement('option');
-                // Fill option with voice and language
-                option.textContent = voice.name + '(' + voice.lang + ')';
-                // Set needed option attributes
-                option.setAttribute('data-lang', voice.lang);
-                option.setAttribute('data-name', voice.name);
-                voiceSelect.appendChild(option);
-            });
+            // // Loop through voices and create an option for each one
+            // voices.forEach(voice => {
+            //     // Create option element
+            //     const option = document.createElement('option');
+            //     // Fill option with voice and language
+            //     option.textContent = voice.name + '(' + voice.lang + ')';
+            //     // Set needed option attributes
+            //     option.setAttribute('data-lang', voice.lang);
+            //     option.setAttribute('data-name', voice.name);
+            //     voiceSelect.appendChild(option);
+            // });
         };
 
         getVoices();
 
-        if (synth.onvoiceschanged !== undefined) {
-            console.log("onvoiceschanged")
+        // if (synth.onvoiceschanged !== undefined) {
+        //     console.log("onvoiceschanged")
             synth.onvoiceschanged = getVoices;
-        }
+        // }
 
 
         recognition.lang = speechcode;
